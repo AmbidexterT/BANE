5b1fb2c154ecae86a3147611a6f2e527726fd851
Stepan <stepan210699@gmail.com>
2022-10-23 12:20:45 +0300

    new

---
+++ src/app/home/home-typing/home-typing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home-typing',
+  templateUrl: './home-typing.component.html',
+  styleUrls: ['./home-typing.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HomeTypingComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
--- src/app/home/home-typing/home-typing.module.ts
+++ src/app/home/home-typing/home-typing.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { HomeTypingModule } from './home-typing/home-typing.module';
+import { HomeTypingComponent } from './home-typing.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HomeRoutingModule,
     MatCardModule,
     MatButtonModule,
-    HomeTypingModule
   ],
   declarations: [
-    HomeComponent
+    HomeTypingComponent,
+  ],
+  exports: [
+    HomeTypingComponent,
   ],
 })
-export class HomeModule { }
+export class HomeTypingModule { }
